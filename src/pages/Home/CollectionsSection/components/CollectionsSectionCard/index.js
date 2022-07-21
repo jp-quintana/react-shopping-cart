516b09b18b834f41e405afd34fb0e9c889a41410
jp-quintana <juanquintana1996@gmail.com>
2022-07-21 16:14:43 -0300

    Rearrange Categories base UI

--- src/pages/Home/CollectionsSection/components/CollectionsSectionCard/index.js
+++ src/pages/Home/CollectionsSection/components/CollectionsSectionCard/index.js
@@ -6,7 +6,7 @@ const CollectionsSectionCard = (props) => {
   const { imageTop, imageBottom, title, url } = props;
 
   return (
-    <Link to={url} className={styles.link}>
+    <Link to={url} className={`${styles.link}`}>
       <h3 className={styles.title}>{title}</h3>
       <img className={styles.image_top} src={imageTop} alt="" />
       <img className={styles.image_bottom} src={imageBottom} alt="" />
