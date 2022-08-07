4b5354103e221c235ffad5b2c142c8aecdd76e46
jp-quintana <juanquintana1996@gmail.com>
2022-08-07 16:55:07 -0300

    WIP: Add Product Detail UI & dummy products

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import Product from './Product';
+
+import { DUMMY_PRODUCTS as products } from './data';
 
 const Products = () => {
-  return <div>Products</div>;
+  return <Product products={products} />;
 };
 
 export default Products;
