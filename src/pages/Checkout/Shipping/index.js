bcbfd4264b90596c92966540acfb0cc15d74c7be
jp-quintana <juanquintana1996@gmail.com>
2022-11-19 23:51:43 -0300

    WIP: Shipping Form

--- src/pages/Checkout/Shipping/index.js
+++ src/pages/Checkout/Shipping/index.js
@@ -1,6 +1,26 @@
 import styles from './index.module.scss';
+
 const Shipping = () => {
-  return <div>Shipping</div>;
+  return (
+    <div className={styles.shipping_wrapper}>
+      <form className={styles.shipping_form}>
+        <div>
+          <label>
+            <input type="radio" className={styles.radio_unselected} />
+            <span>Envio estandard (3 - 5 días)</span>
+          </label>
+          <p>$750</p>
+        </div>
+        <div>
+          <label>
+            <input type="radio" className={styles.radio_unselected} />
+            <span>Envio rápido (2 - 3 días)</span>
+          </label>
+          <p>$1500</p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Shipping;
