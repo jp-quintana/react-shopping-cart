4471482827e4388d287d13d626eb86e3a74fbb82
jp-quintana <juanquintana1996@gmail.com>
2022-11-25 00:28:58 -0300

    Fix Step Styles

--- src/pages/Checkout/CheckoutProgression/Step/index.js
+++ src/pages/Checkout/CheckoutProgression/Step/index.js
@@ -14,54 +14,52 @@ const Step = ({ label, url, index, handleSelectStep, currentStep }) => {
   let stepContent;
   let stepStyles;
 
-  if (currentStep === index) {
+  if (url) {
+    stepStyles = styles.item_completed;
+    stepContent = (
+      <Link to={url}>
+        <div className={styles.step_wrapper}>
+          <span className={styles.label}>{label}</span>
+          <div className={styles.item_completed}>
+            <FaCheck />
+          </div>
+        </div>
+      </Link>
+    );
+  } else if (currentStep === index) {
     stepStyles = styles.item_selected;
-    stepContent = <div className={stepStyles}>{index + 1}</div>;
+    stepContent = (
+      <div className={styles.step_wrapper}>
+        <span className={styles.label}>{label}</span>
+        <div className={stepStyles}>{index + 1}</div>
+      </div>
+    );
   } else if (currentStep > index) {
     stepStyles = styles.item_completed;
     stepContent = (
-      <div onClick={handleClick} className={stepStyles}>
-        <FaCheck />
+      <div onClick={handleClick} className={styles.step_wrapper}>
+        <span className={styles.label}>{label}</span>
+        <div className={stepStyles}>
+          <FaCheck className={styles.checkmark} />
+        </div>
       </div>
     );
   } else {
     stepStyles = styles.item;
-    stepContent = <div className={stepStyles}>{index + 1}</div>;
-  }
-
-  if (url) {
-    return (
-      <>
-        <Link to={url}>
-          <div className={styles.step_wrapper}>
-            <span className={styles.label}>{label}</span>
-            <div className={styles.item_completed}>
-              <FaCheck />
-            </div>
-          </div>
-        </Link>
-        {!lastStep && (
-          <i className={styles.arrow}>
-            <FaChevronRight></FaChevronRight>{' '}
-          </i>
-        )}
-      </>
+    stepContent = (
+      <div className={styles.step_wrapper}>
+        <span className={styles.label}>{label}</span>
+        <div className={stepStyles}>{index + 1}</div>
+      </div>
     );
   }
 
   return (
     <>
-      <div className={styles.step_wrapper}>
-        <span className={styles.label}>{label}</span>
-        {stepContent}
-        {/* <div className={styles.item}>{index + 1}</div> */}
-        {/* <div className={styles.item_completed}>
-          <FaCheck />
-        </div> */}
-      </div>
+      {stepContent}
       {!lastStep && (
         <i className={styles.arrow}>
-          <FaChevronRight></FaChevronRight>{' '}
+          <FaChevronRight />{' '}
         </i>
       )}
     </>
