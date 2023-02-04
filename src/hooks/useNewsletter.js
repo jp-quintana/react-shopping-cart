9dfff3989337de2cacf4381141386580b9e177f5
jp-quintana <juanquintana1996@gmail.com>
2023-02-04 15:42:09 -0300

    WIP: Store Email For Newsletter

---
+++ src/hooks/useNewsletter.js
@@ -0,0 +1,5 @@
+export const useNewsletter = () => {
+  const subscribeToNewsletter = async ({ email }) => {};
+
+  return { subscribeToNewsletter };
+};
--- src/pages/Home/NewsletterSection/index.js
+++ src/pages/Home/NewsletterSection/index.js
@@ -1,29 +1,40 @@
+import { useRef } from 'react';
+
+import { useNewsletter } from 'hooks/useNewsletter';
+
 import Button from 'components/Button';
+
 import styles from './index.module.scss';
 
 const NewsletterSection = () => {
-  const newsletterSubmitHandler = (e) => {
-    e.preventDefault();
-  };
+  const { subscribeToNewsletter } = useNewsletter();
 
-  // TODO: AGREGARLE FUNCIONAMIENTO AL BOTON DE ENVIO
+  const emailInputRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = emailInputRef.current.value;
+
+    console.log(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[A-Za-z]+$/.test(email));
+
+    // await subscribeToNewsletter({});
+  };
 
   return (
     <section className={styles.section}>
       <div className={`${styles.container} main-container`}>
         <h3 className={styles.title}>Subscribite a nuestra newsletter</h3>
         <div>
-          <form
-            className={styles.form}
-            onSubmit={newsletterSubmitHandler}
-            action=""
-          >
+          <form className={styles.form} onSubmit={handleSubmit} action="">
             <input
               className={styles.input}
               placeholder="Tu Dirección de Email"
-              type="text"
+              type="email"
+              ref={emailInputRef}
+              required
             />
-            <Button type="submit" className={`${styles.button} disabled-link`}>
+            <Button type="submit" className={styles.button}>
               Enviar
             </Button>
           </form>
