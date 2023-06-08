f17b0bf9097a643e56f240f262b62242fa6fa5ab
jp-quintana <juanquintana1996@gmail.com>
2023-06-08 16:00:06 -0300

    WIP8: Refactor

---
+++ src/components/common/Image/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Image = ({
+//   src,
+//   alt,
+//   onLoad,
+//   placeholderClassName,
+//   imageClassHolder,
+// }) => {
+//   return (
+//     <div className={styles.image_container}>
+//       {imageIsLoading && <div className={placeholderClassName} />}
+//       <img src={src} onLoad={onLoad} alt={alt} className={imageClassHolder} />
+//     </div>
+//   );
+};
+
+export default Image;
--- src/components/pages/collections/ProductCard/index.jsx
+++ src/components/pages/collections/ProductCard/index.jsx
@@ -27,36 +27,61 @@ const ProductCard = ({
   const isAdmin = location.pathname.split('/')[1] === 'admin';
 
   const [imageIsLoading, setImageIsLoading] = useState(true);
+  const [showImagePlaceholder, setImageShowPlaceholder] = useState(true);
+
+  const clearImagePlaceholder = () => {
+    setImageShowPlaceholder(false);
+    setTimeout(() => {
+      return setImageIsLoading(false);
+    }, 100);
+  };
 
   return (
     <>
       <div className={styles.container}>
-        <Card
-          className={styles.card}
-        >
+        <Card className={styles.card}>
           <Link to={`/products/${slug}`} className={styles.link}>
-            <div className={styles.image_wrapper}>
-              {imageIsLoading && <div className={styles.placeholder_image} />}
+            <div className={styles.image_container}>
+              {imageIsLoading && (
+                <div
+                  className={`${styles.image_placeholder} ${
+                    showImagePlaceholder ? undefined : styles.hide
+                  }`}
+                />
+              )}
               <img
                 src={imageTop.src}
-                onLoad={() => setImageIsLoading(false)}
+                onLoad={clearImagePlaceholder}
                 alt=""
-                className={`${styles.image_primary} ${!imageIsLoading ? styles.loaded : undefined}`}
+                className={`${styles.image_primary} ${
+                  !imageIsLoading ? styles.show : undefined
+                }`}
               ></img>
             </div>
           </Link>
         </Card>
         <ul className={styles.info_wrapper}>
-          <li className={styles.title}>
-            {type} {model}
-          </li>
-          <li className={styles.color}>
-            {color}
-            {numberOfVariants > 1 && (
-              <span>{`${numberOfVariants} colores`}</span>
-            )}
-          </li>
-          <li className={styles.price}>${formatNumber(currentPrice)}</li>
+          {showImagePlaceholder && (
+            <>
+              <li className={styles.title_placeholder} />
+              <li className={styles.color_placeholder} />
+              <li className={styles.price_placeholder} />
+            </>
+          )}
+          {!showImagePlaceholder && (
+            <>
+              <li className={styles.title}>
+                {type} {model}
+              </li>
+              <li className={styles.color}>
+                {color}
+                {numberOfVariants > 1 && (
+                  <span>{`${numberOfVariants} colores`}</span>
+                )}
+              </li>
+              <li className={styles.price}>${formatNumber(currentPrice)}</li>
+            </>
+          )}
         </ul>
         {isAdmin && (
           <div className={styles.admin_buttons_wrapper}>
