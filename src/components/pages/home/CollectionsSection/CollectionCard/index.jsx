c292afe1f56de794e00b86122a16924f65b3c2ff
jp-quintana <juanquintana1996@gmail.com>
2023-06-12 01:47:39 -0300

    WIP14: Refactor

--- src/components/pages/home/CollectionsSection/CollectionCard/index.jsx
+++ src/components/pages/home/CollectionsSection/CollectionCard/index.jsx
@@ -4,14 +4,17 @@ import ImageContainer from 'components/common/ImageContainer';
 import styles from './index.module.scss';
 
 const CollectionCard = ({ id, image, title, text, url }) => {
-  const contentWrapperStyles =
-    title === 'hoodies'
-      ? `${styles.content_wrapper} ${styles.content_wrapper_alt}`
-      : styles.content_wrapper;
+  const isAlt = title === 'hoodies';
 
   return (
     <div className={styles.card}>
-      <div className={contentWrapperStyles}>
+      <div
+        className={
+          isAlt
+            ? `${styles.content_wrapper} ${styles.content_wrapper_alt}`
+            : styles.content_wrapper
+        }
+      >
         <p className={styles.content_title}>{title}</p>
         <p className={styles.content_subtitle}>{text}</p>
         <Button className={styles.button} to={url}>
@@ -21,7 +24,7 @@ const CollectionCard = ({ id, image, title, text, url }) => {
       {/* <img className={styles.image} src={image} alt="" /> */}
       <ImageContainer
         containerClassName={styles.image_container}
-        fillClassName={styles.image_fill}
+        fillClassName={isAlt ? styles.image_fill_alt : styles.image_fill}
         imageClassName={styles.image}
         src={image}
         alt=""
